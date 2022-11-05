03b1a304f2a2042039e38de4fa44f36faef07874
aliapirruccello <alia.pirruccello@gmail.com>
2022-11-05 13:34:58 -0400

    changes

--- config/tailwind.js
+++ config/tailwind.js
@@ -2,12 +2,3 @@ module.exports = {
     //...
     plugins: [require("daisyui")],
   }
-
-  module.exports = {
-    purge: [],
-    theme: {
-      extend: {},
-    },
-    variants: {},
-    plugins: [],
-  }
