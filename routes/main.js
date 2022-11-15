773a55a478c3d3f1feadb09c7b78176523836bab
aliapirruccello <alia.pirruccello@gmail.com>
2022-11-15 16:29:16 -0500

    updated CSS

--- routes/main.js
+++ routes/main.js
@@ -4,6 +4,7 @@ const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const postsController = require("../controllers/posts");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
+const passport = require('passport')
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
@@ -17,5 +18,27 @@ router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
+
+//@desc Auth with Google
+//@Route GET /auth/google
+router.get('/google', passport.authenticate('google', { scope: ['profile']}))
+
+//@desc Google auth callback
+//@Route GET /auth/google/callback
+router.get('/google/callback', 
+passport.authenticate('google', {failureRedirect: '/'}), (req,res) => {
+    res.redirect('/dashboard')
+}
+)
+
+//@desc Logout User
+//@route /auth/logout
+//!Change: Passport 0.6 requires logout to be async
+router.get('/logout', (req,res) => {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+      });
+})
 
 module.exports = router;
